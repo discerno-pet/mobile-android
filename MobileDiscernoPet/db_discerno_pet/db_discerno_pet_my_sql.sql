6a5995ef336d2d48a6dabf77f45e0ab01a2ac6db
Vinicius <vinicius_andrade2010@hotmail.com>
2024-05-31 17:27:13 -0300

    fix: db_discerno_pet em mysql e reverse enginnering

--- MobileDiscernoPet/db_discerno_pet/db_discerno_pet_my_sql.sql
+++ MobileDiscernoPet/db_discerno_pet/db_discerno_pet_my_sql.sql
@@ -6,20 +6,41 @@ USE db_discerno_pet;
 CREATE TABLE IF NOT EXISTS tb_usuario
 (
     id                BIGINT UNSIGNED AUTO_INCREMENT,
-    nome              VARCHAR(100) NOT NULL,
-    email             VARCHAR(100) NOT NULL,
-    email_recuperacao VARCHAR(100) NOT NULL,
-    senha             VARCHAR(100) NOT NULL,
+    email             VARCHAR(100),
     telefone          VARCHAR(20),
-    cep               VARCHAR(20),
-    cpf               VARCHAR(20),
+    senha             VARCHAR(100)                         NOT NULL,
+    tipo_autenticacao ENUM ('email', 'telefone', 'google') NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS tb_usuario_detalhes
+(
+    id                BIGINT UNSIGNED AUTO_INCREMENT,
+    usuario_id        BIGINT UNSIGNED UNIQUE NOT NULL,
+    nome              VARCHAR(100)           NOT NULL,
+    email_recuperacao VARCHAR(100),
+    cpf               VARCHAR(20) UNIQUE,
     foto              VARCHAR(5000),
     nascimento        DATE,
 
-    UNIQUE (email),
-    UNIQUE (cpf),
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuario_id) REFERENCES tb_usuario (id)
+);
 
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS tb_endereco
+(
+    id          BIGINT UNSIGNED AUTO_INCREMENT,
+    usuario_id  BIGINT UNSIGNED UNIQUE NOT NULL,
+    cep         VARCHAR(20)            NOT NULL,
+    logradouro  VARCHAR(200)           NOT NULL,
+    complemento VARCHAR(50),
+    bairro      VARCHAR(100)           NOT NULL,
+    cidade      VARCHAR(100)           NOT NULL,
+    estado      VARCHAR(100)           NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuario_id) REFERENCES tb_usuario (id)
 );
 
 CREATE TABLE IF NOT EXISTS tb_raca
@@ -55,70 +76,3 @@ CREATE TABLE IF NOT EXISTS tb_pet
     FOREIGN KEY (usuario_id) REFERENCES tb_usuario (id),
     FOREIGN KEY (pet_treinamento_ia_id) REFERENCES tb_pet_treinamento_ia (id)
 );
-
-CREATE TABLE IF NOT EXISTS tb_cadastro_cnpj
-(
-    id                BIGINT UNSIGNED AUTO_INCREMENT,
-    nome              VARCHAR(100) NOT NULL,
-    cnpj              VARCHAR(20)  NOT NULL,
-    email             VARCHAR(100) NOT NULL,
-    email_recuperacao VARCHAR(100) NOT NULL,
-    senha             VARCHAR(100) NOT NULL,
-    foto              VARCHAR(5000),
-    contato1          VARCHAR(20),
-    contato2          VARCHAR(20),
-    contato3          VARCHAR(20),
-
-    UNIQUE (cnpj),
-    UNIQUE (email),
-
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS tb_endereco
-(
-    id          BIGINT UNSIGNED AUTO_INCREMENT,
-    logradouro  VARCHAR(200) NOT NULL,
-    numero      VARCHAR(10)  NOT NULL,
-    complemento VARCHAR(50),
-    bairro      VARCHAR(100) NOT NULL,
-    cidade      VARCHAR(100) NOT NULL,
-    estado      VARCHAR(100) NOT NULL,
-    cep         VARCHAR(20)  NOT NULL,
-
-    PRIMARY KEY (id)
-);
-
-# Modelagem DB Discerno PET 🐶
-
-# Usuário
-# 1 - nome
-# 2 - email
-# 3 - emailRecuperacao
-# 4 - senha
-# 5 - telefone (opcional)
-# 6 - endereço (opcional?)
-# 7 - cpf (opcional)
-# 8 - foto(opcional)
-# 9 - Nascimento(opcional)
-#
-# Pet  (a ser cadastrado)
-# 1 - Foto (png/jpeg/outro)
-# 2 - Nome
-# 3 - Idade(Nascimenti)
-# 4 - Raça (previamente cadastradas)
-#
-# Pet (treinamento Ia)
-# 1-Nome
-# 2-Foto (png/jpeg/outro)
-#
-# CadastroCNPJ - (PetShops/Veterinários/etc)
-# 1 - Nome
-# 2 - CNPJ (Obrigatório)
-# 3 - Email
-# 4 - EmailRecuperacao
-# 5 - Senha
-# 6 - Contato1
-# 7 - Contato2
-# 8 - Contato3
-# 9 - Foto (png/jpeg/outro)
